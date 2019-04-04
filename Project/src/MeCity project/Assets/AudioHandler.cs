a3814d840568fae4fd04ec8907ac1d29a7afdbb5
Sam Van Bogaert <sam.van.bogaert@hotmail.com>
2019-04-04 10:45:47 +0200

    Music changes when switched between level or menu accordingly

--- Project/src/MeCity project/Assets/AudioHandler.cs
+++ Project/src/MeCity project/Assets/AudioHandler.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioHandler : MonoBehaviour
 {
     static AudioHandler instance = null;
-
+    [SerializeField] private AudioClip menuMusic;
+    [SerializeField] private AudioClip levelMusic;
+    [SerializeField] private AudioSource source;
+    private string scene;
     // Start is called before the first frame update
     void Awake()
     {
+        scene = SceneManager.GetActiveScene().name;
         if (instance != null)
         {
             Destroy(gameObject);
@@ -21,15 +26,92 @@ public class AudioHandler : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if(scene != SceneManager.GetActiveScene().name)
+        {
+            scene = SceneManager.GetActiveScene().name;
+            //checks if the scene is a lever or not
+            //add new level names to this list
+            if (scene.IsOneOf("Producer","TGO","DGO","Supplier","Consumer"))
+            {
+                PlayLevelMusic();
+            }
+            else
+            {
+                PlayMenuMusic();
+            }
+        }
+    }
+
     public void ToggleSound()
     {
         if (PlayerPrefs.GetInt("Muted", 0) == 0)
         {
             PlayerPrefs.SetInt("Muted", 1);
+            source.volume = 1;
         }
         else
         {
             PlayerPrefs.SetInt("Muted", 0);
+            source.volume = 0;
         }
     }
+
+    public void PlayMenuMusicStatic()
+    {
+        if(instance != null)
+        {
+            if(instance.source != null)
+            {
+                instance.source.Stop();
+                instance.source.clip = instance.menuMusic;
+                instance.source.Play();
+            }
+        }
+    }
+
+    //wrapper method for PlayMenuMusicStatic
+    public void PlayMenuMusic()
+    {
+        PlayMenuMusicStatic();
+    }
+
+    public void PlayLevelMusicStatic()
+    {
+        if (instance != null)
+        {
+            if (instance.source != null)
+            {
+                instance.source.Stop();
+                instance.source.clip = instance.levelMusic;
+                instance.source.Play();
+            }
+        }
+    }
+
+    //wrapper method for PlayLevelMusicStatic
+    public void PlayLevelMusic()
+    {
+        PlayLevelMusicStatic();
+    } 
 }
+
+public static class Helper
+{
+    public static bool IsOneOf<T>(this T value, params T[] items)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i].Equals(value))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
--- Project/src/MeCity project/Assets/scripts/audioMute.cs
+++ Project/src/MeCity project/Assets/scripts/audioMute.cs
@@ -5,36 +5,18 @@ public class audioMute : MonoBehaviour {
 
     private AudioHandler audioHandler;
     public Button btn;
-    public AudioSource audio;
     public RawImage img;
     public Texture mute;
     public Texture unmute;
-    private static bool isMuted = false;
 
     // script used for the mute/unmute audio button
     // first set the image texture to unmute
 	void Start () {
         audioHandler = FindObjectOfType<AudioHandler>();
-        isMuted = false;
+        UpdateIcon();
         btn.onClick.AddListener(PauseMusic);
-        img.texture = unmute;
 	}
     // Muting and unmuting the audio and changing the image texture
-	public void Task()
-    {
-        if (!isMuted)
-        {
-            audio.mute = true;
-            img.texture = mute;
-            isMuted = true;
-        } else
-        {
-            audio.mute = false;
-            img.texture = unmute;
-            isMuted = false;
-        }
-    }
-
     public void PauseMusic()
     {
         audioHandler.ToggleSound();
@@ -43,14 +25,12 @@ public class audioMute : MonoBehaviour {
 
     void UpdateIcon()
     {
-        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
         {
-            audio.volume = 1;
             img.texture = unmute;
         }
         else
         {
-            audio.volume = 0;
             img.texture = mute;
         }
     }
