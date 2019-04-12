a385d68965cd0788b88ba47379a0a19df47e203b
Sam Van Bogaert <sam.van.bogaert@hotmail.com>
2019-04-12 15:16:41 +0200

    Updated scriptie & daily log + started on catcher minigame

---
+++ Project/src/MeCity project/Assets/TGOCatcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TGOCatcher : MonoBehaviour
+{
+    public GameObject lightningPrefab;
+    public GameObject catcher;
+    public GameObject bottomWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    void ResetGame()
+    {
+
+    }
+}
