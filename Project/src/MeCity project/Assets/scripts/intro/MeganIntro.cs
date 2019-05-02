f625de3bde580e8229f404a9c2122e1d804d2a85
Sam Van Bogaert <sam.van.bogaert@hotmail.com>
2019-05-02 09:15:39 +0200

    Edited some intro's and some dilemma's to better fit the game

--- Project/src/MeCity project/Assets/scripts/intro/MeganIntro.cs
+++ Project/src/MeCity project/Assets/scripts/intro/MeganIntro.cs
@@ -16,12 +16,11 @@ public class MeganIntro : MonoBehaviour {
     {
         btn.onClick.RemoveAllListeners();
         btn.onClick.AddListener(LoadNext);
-        txt.text = "This game is developed by 3 students in less then a month time. Goal of the game is to understand how the energy market works," +
-            " which roles there are and who is responsible for what. We want to add this to the onboarding program for new employees and it will be a " +
-            "fun thing to use on the BOOST 18 event as well. The game won't be complete yet, we are focusing on the introduction level and supplier level " +
-            "for this months assignment. \n\n There even might be some bugs left. Our students (Laura, Ryan and Jordan) did not have money resources to make this" +
-            " game and therefore had to use free assets. (you can see this by how fancy the main screen looks in comparison with the ingame graphics. So we" +
-            " count on your constructive and positive attitude to play the game and give contructive feedback to our gamification team.";
+        txt.text = "This game is developed by students in the context of the on boarding program for new employees. Goal of the game is to understand how the energy market works, " +
+            "which roles there are and who is responsible for what."+
+            "\n\nThere ought to be some bugs left, " +
+            "so we count on your constructive and positive attitude to play the game and give contructive feedback through the support page in the game, or mention it directly to the gamification team. " +
+            "Also, don't forget to have fun and enjoy your playthrough!";
     }
     // load the introduction level scene
     public void LoadNext()
